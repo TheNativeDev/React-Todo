534f4226ade4b3b9f51019a7df98728510a46fc3
LezaJackson <53628558+LezaJackson@users.noreply.github.com>
2019-12-03 16:09:08 -0800

    todo

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,44 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 
-
+const styles = theme => ({
+  textField: {
+    width: "100%"
+  },
+  resize: {
+    fontSize: "20px"
+  },
+  buttonDiv: {
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  button: {
+    fontSize: "16px",
+    width: "220px",
+    marginLeft: "10px"
+  }
+});
 
 class TodoForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            task: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
 
-    handleChanges = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    }
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  };
 
-    handleSubmit = e =>{
-        e.preventDefault();
-        if(this.state.task !== ''){
-            this.props.addTask(this.state.task);
-            this.setState({task: ''})
-            e.target.task.value = '';
-        }else{
-            alert('please add a task first');
-        }
-        
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleSubmit(this.state.value);
+    this.setState({ value: "" });
+  };
 
-    render() {
-        return (
-            <div className="task-form-wrapper">
-                <h2>React Task Tracker</h2>
-                <form className="task-form" onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.task} name="task" placeholder="task to add" onChange={this.handleChanges} />
-                    <button>Add Task</button>    
-                </form>
-                <button onClick={this.props.clearCompletedTasks}>Clear Completed Tasks</button>
-            </div>
-        );
-    }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <TextField
+          id="outlined-name"
+          label="Type a todo item"
+          value={this.state.value}
+          onChange={this.handleChange}
+          margin="normal"
+          variant="outlined"
+          className={classes.textField}
+          InputProps={{
+            classes: { input: classes.resize }
+          }}
+          InputLabelProps={{
+            classes: { root: classes.resize }
+          }}
+        />
+        <div className={classes.buttonDiv}>
+          <Button
+            variant="contained"
+            onClick={this.props.handleClear}
+            color="secondary"
+            className={classes.button}
+          >
+            Clear Completed
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Add
+          </Button>
+        </div>
+      </form>
+    );
+  }
 }
-export default TodoForm;
+
+export default withStyles(styles)(TodoForm);
